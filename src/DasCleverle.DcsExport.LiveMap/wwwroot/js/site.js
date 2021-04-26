7cd11a4dbbf0fe96850114824f22e6981250f5cb
DasCleverle <dascleverle@gmail.com>
2021-04-26 23:07:12 +0200

    Add basic handling of 'SendLog' from LiveMapHub

--- src/DasCleverle.DcsExport.LiveMap/wwwroot/js/site.js
+++ src/DasCleverle.DcsExport.LiveMap/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿(async function () {
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl('/hub/livemap')
+    .build();
 
-// Write your JavaScript code.
+  connection.on('SendLog', (request) => {
+    $('#log').append(`<li>${request.message}</li>`);
+  });
+
+  await connection.start();
+})();

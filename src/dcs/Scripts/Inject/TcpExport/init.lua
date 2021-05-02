f32a732aa29eb359a07f47542533ace6b5689012
DasCleverle <dascleverle@gmail.com>
2021-05-02 22:23:52 +0200

    Cleanup code duplication in world event handlers

--- src/dcs/Scripts/Inject/TcpExport/init.lua
+++ src/dcs/Scripts/Inject/TcpExport/init.lua
@@ -1,35 +1,43 @@
 local config = require("config")
 local info = require("info")
 local exporter = require("exporter")
+local logger = require("logger")
 
 -- DCS WORLD EVENTS --
 
+local function addUnitHandler(event)
+    if event == nil or event.initiator == nil then
+        return
+    end
+
+    exporter.send("AddUnit", info.getUnit(event.initiator))
+end
+
+local function removeUnitHandler(event)
+    if event == nil or event.initiator == nil then
+        return
+    end
+
+    exporter.send("RemoveUnit", { id = event.initiator:getID() })
+
+end
+
+local function missionEndHandler(event)
+    logger.info("Received mission end event cleaning up and closing export connection")
+
+    exporter.send("MissionEnd", { time = event.time })
+    exporter.close()
+end
+
 local eventHandlers = {
-    [world.event.S_EVENT_BIRTH] = function (event)
-        exporter.send("AddUnit", info.getUnit(event.initiator))
-    end,
-
-    [world.event.S_EVENT_PLAYER_LEAVE_UNIT] = function (event)
-        exporter.send("RemoveUnit", { id = event.initiator:getID() })
-    end,
-
-    [world.event.S_EVENT_KILL] = function (event)
-        exporter.send("RemoveUnit", { id = event.initiator:getID() })
-    end,
-
-    [world.event.S_EVENT_UNIT_LOST] = function (event)
-        exporter.send("RemoveUnit", { id = event.initiator:getID() })
-    end,
-
-    [world.event.S_EVENT_MISSION_END] = function (event)
-        exporter.send("MissionEnd", { time = event.time })
-        exporter.close()
-    end,
+    [world.event.S_EVENT_BIRTH] = addUnitHandler,
+    [world.event.S_EVENT_PLAYER_LEAVE_UNIT] = removeUnitHandler,
+    [world.event.S_EVENT_KILL] = removeUnitHandler,
+    [world.event.S_EVENT_UNIT_LOST] = removeUnitHandler,
+    [world.event.S_EVENT_MISSION_END] = missionEndHandler,
 
     -- S_EVENT_REMOVE_UNIT (added by MOOSE)
-    [world.event.S_EVENT_MAX + 1006] = function (event)
-        exporter.send("RemoveUnit", { id = event.initiator:getID() })
-    end,
+    [world.event.S_EVENT_MAX + 1006] = removeUnitHandler,
 }
 
 function eventHandlers:onEvent(event)
@@ -72,6 +80,8 @@ end
 timer.scheduleFunction(updatePositions, nil, timer.getTime() + 10)
 
 -- INITIAL EXPORT ON MISSION INIT --
+logger.info("Starting up tcp export. Connecting and sending initial information")
+
 exporter.connect()
 exporter.send("Init", { time = timer.getTime() }) -- TODO: Report data like weather and time of day
 exporter.send("AddUnit", info.getAllUnits())

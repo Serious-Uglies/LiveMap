65353b86179380be5e506d8ac744aa74e2461bfb
DasCleverle <dascleverle@gmail.com>
2021-05-14 14:19:22 +0200

    Refactor template.js to allow for conditionals

--- src/server/DasCleverle.DcsExport.LiveMap/wwwroot/js/template.js
+++ src/server/DasCleverle.DcsExport.LiveMap/wwwroot/js/template.js
@@ -1,68 +1,90 @@
 (function ($) {
-  const replaceParams = (content, params) => {
-    const paramRe = /\$\{(\w+)(\.(\w+))?\}/g;
+  const evalExpression = (expr, params) => {
+    const keys = Object.keys(params);
+    const values = Object.values(params);
+    const fn = new Function(
+      `return function(${keys.join(',')}) { return ${expr}; }`
+    )();
 
-    return content.replace(paramRe, (_, name, __, property) => {
-      let value = params[name];
+    return fn.apply(fn, values);
+  };
 
-      if (property !== undefined) {
-        value = value[property];
+  const evalParams = ($el, params) => {
+    const paramRe = /\$\{(.+?)\}/g;
+    return he
+      .decode($el.html())
+      .replace(paramRe, (_, expr) => evalExpression(expr, params));
+  };
+
+  const evalConditional = ($el, params) => {
+    const expr = $el.attr('$if');
+    const result = evalExpression(expr, params);
+
+    $el.removeAttr('$if');
+
+    return result;
+  };
+
+  const evalLoop = ($el, params) => {
+    const match = /(\w+) of (\w+)/.exec($el.attr('$for'));
+    const variable = match[1];
+    const iteratable = params[match[2]];
+
+    $el.removeAttr('$for');
+
+    const template = $el.html();
+    const result = [];
+
+    for (const item of iteratable) {
+      const innerParams = Object.assign({}, params, {
+        [variable]: item,
+      });
+
+      result.push(evalTemplate(template, innerParams));
+    }
+
+    return result.join('');
+  };
+
+  const evalTemplate = (template, params) => {
+    const children = $('<div />').html(template).children();
+
+    if (children.length == 0) {
+      return template;
+    }
+
+    const result = [];
+
+    children.each((_, el) => {
+      const $el = $(el);
+
+      if ($el.attr('$if') && !evalConditional($el, params)) {
+        return;
       }
 
-      return value;
+      if ($el.attr('$for')) {
+        $el.html(evalLoop($el, params));
+      }
+
+      $el.html(evalParams($el, params));
+
+      const html = $el.html();
+
+      if (html.includes('$for') || html.includes('$if')) {
+        $el.html(evalTemplate(html, params));
+      }
+
+      result.push(el.outerHTML);
     });
+
+    return result.join('');
   };
 
-  const runLoops = (content, params) => {
-    if (!content.includes('$for')) {
-      return content;
-    }
-
-    const forRe = /(\w+) of (\w+)/;
-    const executeLoop = (el) => {
-      const match = forRe.exec(el.attributes['$for'].value);
-      const variable = match[1];
-      const arrayName = match[2];
-
-      const $el = $(el);
-      const tpl = $el.html();
-      const array = params[arrayName];
-
-      const result = [];
-
-      array.forEach((item) => {
-        const innerParams = Object.assign({}, params, {
-          [variable]: item,
-        });
-
-        result.push(replaceParams(tpl, innerParams));
-      });
-
-      $el.removeAttr('$for');
-      $el.html(result.join(''));
-    };
-
-    const $content = $('<div />').html(content);
-
-    if ($content.attr('$for')) {
-      executeLoop($content[0]);
-    } else {
-      $content.find('[\\$for]').each((_, el) => {
-        executeLoop(el);
-      });
-    }
-
-    return $content.html();
-  };
-
-  $.fn.template = function (template, handlers, params) {
+  $.fn.template = function (template, params, handlers) {
     const content = $(`script[data-template="${template}"]`).html();
     const target = $(this);
 
-    const withLoops = runLoops(content, params);
-    const withParams = replaceParams(withLoops, params);
-
-    target.html(withParams);
+    target.html(evalTemplate(content, params));
 
     if (handlers) {
       target.find(`[data-event][data-handler]`).each((_, el) => {

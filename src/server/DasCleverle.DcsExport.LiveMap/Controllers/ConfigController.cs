ee6c688f91cf839979cf0e4d804d4021623122c8
DasCleverle <dascleverle@gmail.com>
2021-06-03 17:40:21 +0200

    Add config controller in prep for React app

---
+++ src/server/DasCleverle.DcsExport.LiveMap/Controllers/ConfigController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DasCleverle.DcsExport.LiveMap.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConfigController : Controller
+    {
+        [HttpGet("mapbox")]
+        public MapboxOptions GetMapboxConfig([FromServices] IOptionsSnapshot<MapboxOptions> options)
+        {
+            return options.Value;
+        }
+    }
+}

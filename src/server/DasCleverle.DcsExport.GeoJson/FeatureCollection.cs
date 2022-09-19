7200186b7a766eb9135847d7d2f38b9a534162b6
DasCleverle <dascleverle@gmail.com>
2022-09-19 22:27:39 +0200

    Add mutation methods to FeatureCollection

--- src/server/DasCleverle.DcsExport.GeoJson/FeatureCollection.cs
+++ src/server/DasCleverle.DcsExport.GeoJson/FeatureCollection.cs
@@ -6,20 +6,45 @@ using DasCleverle.DcsExport.GeoJson.Json;
 namespace DasCleverle.DcsExport.GeoJson;
 
 [JsonConverter(typeof(JsonFeatureCollectionConverter))]
-public record FeatureCollection : IEnumerable<IFeature>
+public record FeatureCollection : IEnumerable<Feature>
 {
     public GeoJsonType Type => GeoJsonType.FeatureCollection;
 
-    public ImmutableList<IFeature> Features { get; init; } = ImmutableList<IFeature>.Empty;
+    public ImmutableList<Feature> Features { get; init; } = ImmutableList<Feature>.Empty;
 
     public FeatureCollection() { }
 
-    public FeatureCollection(IEnumerable<IFeature> features)
+    public FeatureCollection(IEnumerable<Feature> features)
     {
         Features = ImmutableList.CreateRange(features);
     }
 
-    public IEnumerator<IFeature> GetEnumerator() => Features.GetEnumerator();
+    public FeatureCollection Add(Feature feature) => this with { Features = Features.Add(feature) };
+
+    public FeatureCollection AddRange(IEnumerable<Feature> features) => this with { Features = Features.AddRange(features) };
+
+    public FeatureCollection Remove(Feature feature) => this with { Features = Features.Remove(feature) };
+
+    public FeatureCollection Remove(string id) => this with { Features = Features.RemoveAll(x => x.Id == id) };
+
+    public FeatureCollection Update(string id, Func<Feature, Feature> updater)
+    {
+        var feature = Find(id);
+
+        if (feature == null)
+        {
+            return this;
+        }
+
+        return this with
+        {
+            Features = Features.Replace(feature, updater(feature))
+        };
+    }
+
+    public Feature? Find(string id) => Features.Find(x => x.Id == id);
+
+    public IEnumerator<Feature> GetEnumerator() => Features.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => Features.GetEnumerator();
 }

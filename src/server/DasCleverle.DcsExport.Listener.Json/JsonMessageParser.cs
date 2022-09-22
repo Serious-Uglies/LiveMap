fd3ea8b82403791124d63f266ddbb3ea26101f1d
DasCleverle <dascleverle@gmail.com>
2022-09-22 22:06:21 +0200

    Remove redundant logging from JsonMessageParser

--- src/server/DasCleverle.DcsExport.Listener.Json/JsonMessageParser.cs
+++ src/server/DasCleverle.DcsExport.Listener.Json/JsonMessageParser.cs
@@ -1,30 +1,16 @@
 using System.Buffers;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using DasCleverle.DcsExport.Listener.Abstractions;
-using Microsoft.Extensions.Logging;
 
 namespace DasCleverle.DcsExport.Listener.Json;
 
 public class JsonMessageParser : IMessageParser
 {
-    private static readonly JsonSerializerOptions Options = GetOptions();
-    private readonly ILogger<JsonMessageParser> _logger;
-
-    public JsonMessageParser(ILogger<JsonMessageParser> logger)
-    {
-        _logger = logger;
-    }
+    internal static readonly JsonSerializerOptions Options = GetOptions();
 
     public Task<IExportEvent> ParseMessageAsync(ReadOnlySequence<byte> message, CancellationToken token)
     {
-        if (_logger.IsEnabled(LogLevel.Trace))
-        {
-            var str = Encoding.ASCII.GetString(message);
-            _logger.LogTrace("Received JSON export event: {Event}", str);
-        }
-
         var reader = new Utf8JsonReader(message);
         var exportEvent = JsonSerializer.Deserialize<IExportEvent>(ref reader, Options)!;
 
